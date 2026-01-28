90de01c6cf9ce693e93904c68b4f187e5c0eac44
lgivhan <youngturkay@gmail.com>
2026-01-28 13:31:01 -0500

    feat: implemented PageFactory locators and verified sendKeys in LoginPage

--- src/test/java/com/lgivhan/automation/pages/LoginPage.java
+++ src/test/java/com/lgivhan/automation/pages/LoginPage.java
@@ -1,14 +1,36 @@
 package com.lgivhan.automation.pages;
 
 import org.springframework.stereotype.Component;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import jakarta.annotation.PostConstruct;
 
 @Component
 public class LoginPage extends BasePage {
 
+    // Locators
+    @FindBy(name = "email")
+    private WebElement usernameField;
+
+    @FindBy(name = "password")
+    private WebElement passwordField;
+
+    @FindBy(css = "button[type='submit']")
+    private WebElement loginButton;
+
+    // Connects @FindBy to the driver
+    @PostConstruct
+    public void init() {
+        PageFactory.initElements(driver, this);
+    }
+
     public void navigateToApp() {
         driver.get("https://lgivhan.github.io/stock-tracker/");
     }
-    public void login(String u, String p) {
-        System.out.println("Browser is open and at the URL");
+    public void login(String user, String pass) {
+        usernameField.sendKeys(user);
+        passwordField.sendKeys(pass);
+        loginButton.click();
     }
 }
